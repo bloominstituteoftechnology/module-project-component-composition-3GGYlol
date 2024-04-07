dc3d000eb06c543c13439f8a9e4124d3789d9787
3GGYlol <xlj135@gmail.com>
2024-04-07 17:48:45 -0400

    wip

--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledFigure = styled.figure`
+  max-width: 60%;
+
+  figcaption {
+   font-style: italic;    
+ }
+`
 
 export default function Figure({ image, author, date }) {
     return (
-      <figure>
+      <StyledFigure>
         <img src={image} />
         <figcaption>
             The Photo of the Day by {author} on {date}
         </figcaption>
-      </figure>
+      </StyledFigure>
     )
   }
